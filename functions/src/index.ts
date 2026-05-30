979244659f6a063ca87c75a80fb49518e9eb2439
Christi05360856 <99998581+Christi05360856@users.noreply.github.com>
2026-05-30 09:43:01 +0100

    Update index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,6 @@ if (!admin.apps.length) { admin.initializeApp(); }
 
 export { createQuizSession }        from './quiz/createSession';
 export { submitQuizSession }        from './quiz/submitSession';
-export { processRewardClaim }       from './rewards/processReward';
-export { archiveWeeklyLeaderboard } from './leaderboard/archiveWeek';
+export { processRewardClaim }       from './rewards/processRewardClaim';
+export { archiveWeeklyLeaderboard } from './leaderboard/archiveWeeklyLeaderboard';
 export { sendDailyReminders }       from './notifications/scheduler';
