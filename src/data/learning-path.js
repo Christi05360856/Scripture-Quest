71022ace8003ceb4a2e6565f334a9235d1a13d49
Christi05360856 <99998581+Christi05360856@users.noreply.github.com>
2026-06-28 00:51:01 +0100

    Update learning-path.js

--- src/data/learning-path.js
+++ src/data/learning-path.js
@@ -23,8 +23,8 @@ import { samuel1 }       from './samuel1.js';
 import { samuel2 }       from './samuel2.js';
 import { kings1 }        from './kings1.js';
 import { kings2 }        from './kings2.js';
-//import { chronicles1 }   from './chronicles1.js';
-//import { chronicles2 }   from './chronicles2.js';
+import { chronicles1 }   from './chronicles1.js';
+import { chronicles2 }   from './chronicles2.js';
 import { ezra }          from './ezra.js';
 import { nehemiah }      from './nehemiah.js';
 import { esther }        from './esther.js';
@@ -113,8 +113,8 @@ export const learningPath = {
   ...samuel2,
   ...kings1,
   ...kings2,
-  //...chronicles1,
-  //...chronicles2,
+  ...chronicles1,
+  ...chronicles2,
   ...ezra,
   ...nehemiah,
   ...esther,
